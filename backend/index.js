9edf7f16d07ecc947b8354e4f75454e3b4db30fd
samadritacoder <samadrita.munshi2003@gmail.com>
2023-09-28 17:00:23 +0530

    fi done

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const bodyparser = require("body-parser")
+const cors = require("cors")
+
+const app = express();
+app.use(bodyparser.json());
+app.use(cors())
+const name = [
+    {
+        email:"samadrita@.com",
+        password: "1234",
+        token : "ygxxg"
+    },
+    {
+        email:"utpal@.com",
+        password: "123",
+        token : "ygx"   
+    },
+    {
+        email:"utpal@.com",
+        password: "12",
+        token : "ygxx"  
+    }
+]
+
+
+
+
+app.post("/login", cors(), (req,res)=>{
+    const newName = name.find(user =>user.email === req.body.email &&  user.password === req.body.password)
+    if (newName) {
+        res.send({
+            succ : true,
+            fail : false,
+           
+        });
+    }else{
+        res.send({
+            succ : true,
+            fail : false,
+            messege : "something is wrong"
+        
+
+        })
+    }
+})
+
+app.listen(3000,()=>{
+    console.log('app is running');
+})
+
